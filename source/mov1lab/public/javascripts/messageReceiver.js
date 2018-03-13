343105b106766ccdbfb42c11f7a21291673af5e6
amoralesn <alejandro.morales.am3@contractors.roche.com>
2018-03-13 12:13:13 +0100

    Updating messageReceiver.js
    Added "-" if the field is empty to show in each new row.

--- source/mov1lab/public/javascripts/messageReceiver.js
+++ source/mov1lab/public/javascripts/messageReceiver.js
@@ -1,12 +1,10 @@
 const MaxRows = 20;
 const DatetimePosition = 1;
 const PatientPosition = 2;
-const TestPosition = 1;
-const InstrumentPosition = 2;
-const SerialPosition = 3;
+const TestPosition = 3;
+const InstrumentPosition = 4;
+const SerialPosition = 5;
 var httpRequest = new XMLHttpRequest();
-var dateTime;
-var patient;
 var lastTimeHeartBeatWasReceived;
 
 httpRequest.onload = function(){
@@ -24,8 +22,6 @@ function startMessageReceive(socket){
     var data = parseHl7message(msg);
     var dataTable = document.getElementById("update-table");
   
-    data.forEach(element => {
-
       if (dataTable.childElementCount >= MaxRows) {
         dataTable.removeChild(dataTable.lastElementChild);
       }
@@ -34,53 +30,41 @@ function startMessageReceive(socket){
 
       var tdDatetime = document.createElement("td");
       tr.appendChild(tdDatetime);
-      tdDatetime.innerText = element.Datetime;
-      tdDatetime.className ="col-md-3 lead";
+      tdDatetime.innerText = data.Datetime;
+      tdDatetime.className ="col-md-3";
 
       var tdPatien = document.createElement("td");
       tr.appendChild(tdPatien);
-      tdPatien.innerText = element.Patient;
-      tdPatien.className ="col-md-2 lead";
+      tdPatien.innerText = data.Patient;
+      tdPatien.className ="col-md-2";
 
       var tdTest = document.createElement("td");
       tr.appendChild(tdTest);
-      tdTest.innerText = element.Test;
-      tdTest.className ="col-md-2 lead";
+      tdTest.innerText = data.Test;
+      tdTest.className ="col-md-2";
 
       var tdInstrument = document.createElement("td");
       tr.appendChild(tdInstrument);
-      tdInstrument.innerText = element.Instrument;
-      tdInstrument.className ="col-md-2 lead";
+      tdInstrument.innerText = data.Instrument;
+      tdInstrument.className ="col-md-2";
 
       var tdSerial = document.createElement("td");
       tr.appendChild(tdSerial);
-      tdSerial.innerText = element.Serial;
-      tdSerial.className ="col-md-3 lead";
+      tdSerial.innerText = data.Serial;
+      tdSerial.className ="col-md-3";
 
-      dataTable.insertBefore(tr,dataTable.firstElementChild);    
-    });
-});
+      dataTable.insertBefore(tr,dataTable.firstElementChild);  
+  });
 
 function parseHl7message(msg) {
   var splitMsg = msg.split("|");
-  dateTime = splitMsg[DatetimePosition];
-  patient = hidePatientId(splitMsg[PatientPosition]);
-
-  var returnValue = [];
-
-  var i=0;
-  while(splitMsg.indexOf("NEWTEST", i) > 0){
-    returnValue.push({ 
-      Datetime: dateTime == '' ? "-" : dateTime, 
-      Patient: patient == '' ? "-" : patient, 
-      Test: splitMsg[splitMsg.indexOf("NEWTEST", i)+TestPosition] == '' ? "-" : splitMsg[splitMsg.indexOf("NEWTEST", i)+TestPosition], 
-      Instrument: splitMsg[splitMsg.indexOf("NEWTEST", i)+InstrumentPosition] == '' ? "-" : splitMsg[splitMsg.indexOf("NEWTEST", i)+InstrumentPosition], 
-      Serial: splitMsg[splitMsg.indexOf("NEWTEST", i)+SerialPosition] == '' ? "-" : splitMsg[splitMsg.indexOf("NEWTEST", i)+SerialPosition] });
-
-    i = splitMsg.indexOf("NEWTEST", i) + 1;
-  }
-
-  return returnValue;
+  return { 
+    Datetime: splitMsg[DatetimePosition] == '' ? "-" : splitMsg[DatetimePosition], 
+    Patient: hidePatientId(splitMsg[PatientPosition] == '' ? "-" : splitMsg[PatientPosition]), 
+    Test: splitMsg[TestPosition] == '' ? "-" : splitMsg[TestPosition], 
+    Instrument: splitMsg[InstrumentPosition] == '' ? "-" : splitMsg[InstrumentPosition],
+    Serial: splitMsg[SerialPosition] == '' ? "-" : splitMsg[SerialPosition]
+  };
 };
 
 socket.on('pong',function(){
